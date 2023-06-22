a61a9f0a05f0ee55d0aba682a76c0461b87153f1
travisyx <travisyx@gmail.com>
2023-06-22 00:21:55 -0700

    Add a template file with line explanations

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> // Allows use of entire standard library
+
+using namespace std; // So we don't have to write std:: in front of everything
+
+int main() {
+    /*
+        The following two lines of code makes I/O more efficient. The default
+        configuration for C++ is that if a C++ stream object (i.e. cin) is
+        mixed with a C stream object (i.e. scanf()), the two streams will be
+        synced, which can be slower. The first line disables this. 
+
+        Then, C++ also generally ties together the input and output streams
+        (cin and cout) together. This means that the program will wait for the
+        output to be flushed and appears before any more input is presented.
+        The second line unties these, which can be faster.
+    */
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // Insert solution here
+}
